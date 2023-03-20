53a30c583520100a5639b631dedd2982f6e153ae
Alec Wassill <awassill@pacesetterdirectional.ca>
2023-03-20 14:37:45 -0600

    Added Program code.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,21 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        // Start here...
+        try
+        {
+            using StreamReader pressureAsStringStreamReader = new(@"..\..\..\Pressure_As_ADC_Data_Strings.txt");
+            using StreamWriter PressureAsVoltageStreamWriter = new(@"..\..\..\Pressure_As_Voltages.txt", true);
+            string line;
+
+            while ((line = pressureAsStringStreamReader.ReadLine()) != null)
+            {
+                // Implement specification.
+            }
+        }
+        catch (IOException ioException)
+        {
+            Console.WriteLine($"IOException: {ioException.Message}");
+            throw;
+        }
     }
 }
